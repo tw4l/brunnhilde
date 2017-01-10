ce5883c6a7a0c8c8f79c941f66288ce748b1b405
Tim Walsh <timothyryanwalsh@gmail.com>
2017-01-10 11:26:24 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name = 'brunnhilde',
+    version = '1.4.0',
+    url = 'https://github.com/timothyryanwalsh/brunnhilde',
+    author = 'Tim Walsh',
+    author_email = 'timothyryanwalsh@gmail.com',
+    py_modules = ['brunnhilde'],
+    scripts = ['brunnhilde.py'],
+    description = 'A Siegfried-based digital archives reporting tool for directories and disk images',
+    keywords = 'archives reporting formats directories diskimages',
+    platforms = ['POSIX'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Natural Language :: English', 
+        'Operating System :: MacOS',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Communications :: File Sharing',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Database',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities'
+    ],
+)
