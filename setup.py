15299d7f15a1aa9523c491190880e1aad84cae07
Tim Walsh <timothyryanwalsh@gmail.com>
2017-08-28 11:01:09 -0400

    Update for 1.6.0 - TODO: Add Windows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'brunnhilde',
-    version = '1.5.4',
+    version = '1.6.0',
     url = 'https://github.com/timothyryanwalsh/brunnhilde',
     author = 'Tim Walsh',
     author_email = 'timothyryanwalsh@gmail.com',
