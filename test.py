def86fed5d7adaeabe83164456d9fb8c1ebfd211
Tim Walsh <timothyryanwalsh@gmail.com>
2017-08-29 17:51:01 -0400

    Add test for non-HFS disk image

--- test.py
+++ test.py
@@ -26,25 +26,17 @@ class SelfCleaningTestCase(unittest.TestCase):
     def setUp(self):
         super(SelfCleaningTestCase, self).setUp()
 
-        # tempdir for sample data
-        self.src_tmpdir = tempfile.mkdtemp()
-        if os.path.isdir(self.src_tmpdir):
-            shutil.rmtree(self.src_tmpdir)
-        shutil.copytree('test-data', self.src_tmpdir)
-
         # tempdir for brunnhilde outputs
         self.dest_tmpdir = tempfile.mkdtemp()
         if not os.path.isdir(self.dest_tmpdir):
             os.mkdirs(self.dest_tmpdir)
 
     def tearDown(self):
-        for temp_dir in self.src_tmpdir, self.dest_tmpdir:
-            if os.path.isdir(temp_dir):
-                shutil.rmtree(temp_dir)
+        if os.path.isdir(self.dest_tmpdir):
+            shutil.rmtree(self.dest_tmpdir)
 
 
         super(SelfCleaningTestCase, self).tearDown()
-
 
 class TestBrunnhildeIntegration(SelfCleaningTestCase):
     """
@@ -52,8 +44,8 @@ class TestBrunnhildeIntegration(SelfCleaningTestCase):
     """
 
     def test_integration_outputs_created(self):
-        subprocess.call("python ./brunnhilde.py %s %s brunnhilde_test" % (self.src_tmpdir, 
-            self.dest_tmpdir), shell=True)
+        subprocess.call('python ./brunnhilde.py ./test-data/files/ "%s" brunnhilde_test' % (self.dest_tmpdir), 
+            shell=True)
         # siegfried csv and sqlite db
         self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
             'siegfried.csv')))
@@ -86,6 +78,47 @@ class TestBrunnhildeIntegration(SelfCleaningTestCase):
         # virus check log
         self.assertTrue(os.path.isfile(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
             'logs', 'viruscheck-log.txt')))
+
+    def test_integration_outputs_created_diskimage(self):
+        subprocess.call('python ./brunnhilde.py -d ./test-data/diskimages/sample-floppy-fat.dd "%s" brunnhilde_test' % (self.dest_tmpdir), 
+            shell=True)
+        # siegfried csv and sqlite db
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'siegfried.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'siegfried.sqlite')))
+        # html report
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'brunnhilde_test.html')))
+        # csv reports
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'duplicates.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'errors.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'formats.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'formatVersions.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'mimetypes.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'unidentified.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'warnings.csv')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'csv_reports', 'years.csv')))
+        # tree.txt
+        if not sys.platform.startswith('win'):
+            self.assertTrue(os.path.isfile(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+                'tree.txt')))
+        # dfxml
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tmpdir, 'brunnhilde_test', 
+            'dfxml.xml')))
+        # carved_files
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tempdir, 'brunnhilde_test', 
+            'carved_files', 'file1.txt.txt')))
+        self.assertTrue(is_non_zero_file(os.path.join(self.dest_tempdir, 'brunnhilde_test', 
+            'carved_files', 'Tulips.jpg')))
     
     def test_integration_temp_files_deleted(self):
         subprocess.call("python ./brunnhilde.py %s %s brunnhilde_test" % (self.src_tmpdir, 
