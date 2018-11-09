be78168a5a0611a33ec00d2eccffca72d88b8c57
Tim Walsh <timothyryanwalsh@gmail.com>
2018-11-08 19:25:44 -0500

    Add .asset assertions for integration tests

--- test.py
+++ test.py
@@ -75,6 +75,16 @@ class TestBrunnhildeIntegration(SelfCleaningTestCase):
         if not sys.platform.startswith('win'):
             self.assertTrue(os.path.isfile(j(self.dest_tmpdir, 'test', 
                 'tree.txt')))
+        # bootstrap css/js
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'test', 
+            '.assets', 'css', 'bootstrap.min.css')))
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'test', 
+            '.assets', 'js', 'bootstrap.min.js')))
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'test', 
+            '.assets', 'js', 'jquery-3.3.1.slim.min.js')))
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'test', 
+            '.assets', 'js', 'popper.min.js')))
+
 
     def test_integration_outputs_created_diskimage(self):
         subprocess.call('python brunnhilde.py -nd ./test-data/diskimages/sample-floppy-fat.dd "%s" test' % (self.dest_tmpdir), 
@@ -167,8 +177,18 @@ class TestBrunnhildeIntegration(SelfCleaningTestCase):
             shell=True)
         subprocess.call('python brunnhilde.py --load_assets "%s" ./test-data/files/ "%s" load-test' % (assets_dir, self.dest_tmpdir), 
             shell=True)
+        # report
         self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'load-test', 
             'report.html')))
+        # bootstrap css/js
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'load-test', 
+            '.assets', 'css', 'bootstrap.min.css')))
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'load-test', 
+            '.assets', 'js', 'bootstrap.min.js')))
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'load-test', 
+            '.assets', 'js', 'jquery-3.3.1.slim.min.js')))
+        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'load-test', 
+            '.assets', 'js', 'popper.min.js')))
 
 
 if __name__ == '__main__':
